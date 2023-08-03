3a87d1a99a91b5408563945e12bcca0ff28dfdfb
imbard <nicota1234@gmail.com>
2023-08-02 21:03:20 -0300

    Sidebar component

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -5,8 +5,19 @@ import { Menu } from "lucide-react";
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, [])
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -15,7 +26,7 @@ const MobileSidebar = () => {
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
-    <Sidebar />
+        <Sidebar />
       </SheetContent>
     </Sheet>
   );
